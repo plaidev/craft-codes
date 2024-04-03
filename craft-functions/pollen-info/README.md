3b6ef2ec57d9b64af4a1b87867a4bc04af8fc52d
UG-Yoshida <126647193+UG-Yoshida@users.noreply.github.com>
2024-04-03 18:26:33 +0900

    commit

---
+++ craft-functions/pollen-info/README.md
@@ -0,0 +1,17 @@
+# Summary
+
+## title
+
+「緯度経度」からCraft Functions × Google Maps PlatformのPollen APIで「花粉情報」を取得してKARTEに連携する
+
+## blogUrl
+
+https://solution.plaid.co.jp/202403-get-pollen-info-with-google-pollen-api
+
+## description
+
+緯度経度の情報を基にCraft functionsからGoogle Maps PlatformのPollen APIと連携して花粉情報を取得してKARTEイベントを発生させます。
+
+## category
+
+Google Maps Platform,Pollen API,Craft Functions,KARTEイベント,接客サービス,CRAFT_ENDPOINT
--- craft-functions/pollen-info/index.js
+++ craft-functions/pollen-info/index.js
@@ -4,17 +4,21 @@ const LOG_LEVEL = '<% LOG_LEVEL %>';
 const KARTE_APP_TOKEN_SECRET = '<% KARTE_APP_TOKEN_SECRET %>';
 const EVENT_NAME = '<% EVENT_NAME% >';
 const GOOGLEMAPSAPIKEY = '<% GOOGLEMAPSAPIKEY %>';
+const FIELD = '<% FIELD %>';
 const karteApiClient = api('@dev-karte/v1.0#1jvnhd6llgekil84');
 
-async function fetchpollenfinfo(latitude, longitude, logger) {
+async function fetchPollenInfo(latitude, longitude, logger) {
   try {
-    const res = await fetch(`https://pollen.googleapis.com/v1/forecast:lookup?key=${GOOGLEMAPSAPIKEY}&location.longitude=${longitude}&location.latitude=${latitude}&days=1`, {
-      method: 'GET',
-    });
-    if (!res.ok) {
-      throw new Error(`Response not OK: ${res.statusText}`);
+    const pollenInfo = await fetch(
+      `https://pollen.googleapis.com/v1/forecast:lookup?key=${GOOGLEMAPSAPIKEY}&location.longitude=${longitude}&location.latitude=${latitude}&days=1`,
+      {
+        method: 'GET',
+      }
+    );
+    if (!pollenInfo.ok) {
+      throw new Error(`Response not OK: ${pollenInfo.statusText}`);
     }
-    const pollen = await res.json();
+    const pollen = await pollenInfo.json();
     return pollen;
   } catch (err) {
     logger.error('fetchに失敗しました:', err);
@@ -22,22 +26,18 @@ async function fetchpollenfinfo(latitude, longitude, logger) {
   }
 }
 
-
 export default async function (data, { MODULES }) {
   const { initLogger, secret } = MODULES;
   const logger = initLogger({ logLevel: LOG_LEVEL });
-  const secrets = await secret.get({ keys: [KARTE_APP_TOKEN_SECRET] });
-  const token = secrets[KARTE_APP_TOKEN_SECRET];
   const body = data.jsonPayload.data.hook_data.body;
-  karteApiClient.auth(token);
   if (!body) {
-    logger.error('bodyの値が存在しません');
+    logger.error('bodyが存在しません');
     return;
   }
 
   const visitorId = body.visitor_id;
   if (!visitorId) {
-    logger.error('vistorIdが存在しません');
+    logger.error('visitor_idが存在しません');
     return;
   }
 
@@ -48,23 +48,27 @@ export default async function (data, { MODULES }) {
     return;
   }
 
-  const pollenInfo = await fetchpollenfinfo(latitude, longitude, logger); 
-  if (!pollenInfo) {
+  const secrets = await secret.get({ keys: [KARTE_APP_TOKEN_SECRET] });
+  const token = secrets[KARTE_APP_TOKEN_SECRET];
+  karteApiClient.auth(token);
+
+  const pollen = await fetchPollenInfo(latitude, longitude, logger);
+  if (!pollen) {
     return;
   }
+  const values = {};
+  values[FIELD] = pollen;
 
   try {
     await karteApiClient.postV2betaTrackEventWriteandexecaction({
       keys: { visitor_id: visitorId },
       event: {
-        event_name:EVENT_NAME,
-        values: {
-          pollen_data: pollenInfo,
-        }
+        event_name: EVENT_NAME,
+        values,
       },
     });
-    logger.log(EVENT_NAME + 'Event sent successfully.');
+    logger.log(`Event sent successfully. event_name: ${EVENT_NAME}`);
   } catch (e) {
-    logger.error(EVENT_NAME + 'was not sent successfully.');
+    logger.error(`send event failed. event_name: ${EVENT_NAME}, error: ${e}`);
   }
 }
