d3a3b6b228d3f71eff27180888a7f9b3a686f29b
KazunoriKamiya <135207527+KazunoriKamiya@users.noreply.github.com>
2024-05-21 23:01:52 +0900

    user_idとline_idマッピング

---
+++ craft-functions/userid-lineid-mapping/README.md
@@ -0,0 +1,17 @@
+# Summary
+
+## title
+
+Craft Functionsを使ってLINE IDとWebサイトのユーザーIDを紐付ける
+
+## blogUrl
+
+https://solution.karte.io/blog/2024/05/userid-lineid-mapping/
+
+## description
+
+LINEログイン時の認可コードを受け取ってLINE IDを取得し、会員IDとともに紐付けテーブルに格納します。
+
+## category
+
+LINE,Craft Functions,紐付けテーブル,接客サービス,CRAFT_ENDPOINT
---
+++ craft-functions/userid-lineid-mapping/index.js
@@ -0,0 +1,141 @@
+import api from 'api';
+
+const LOG_LEVEL = '<% LOG_LEVEL %>';
+const LINE_LOGIN_CHANNEL_ID = '<% LINE_LOGIN_CHANNEL_ID %>'; // LINEログインチャネルのチャネルID
+const LINE_CHANNEL_SECRET_NAME = '<% LINE_CHANNEL_SECRET_NAME %>'; // シークレットマネージャーに登録したLINEログインチャネルシークレットの名前
+const REDIRECT_URI = '<% REDIRECT_URI %>'; // LINEログイン後にリダイレクトされる画面のURL
+const REF_TABLE_ID = '<% REF_TABLE_ID %>'; // 紐付けテーブルのテーブルID
+const KARTE_API_TOKEN_SECRET = '<% KARTE_API_TOKEN_SECRET %>'; // API v2アプリのトークンを登録したシークレット
+
+const sdk = api('@dev-karte/v1.0#4013y24lvyu582u');
+
+// Webサイトから取得した認可コードを使ってIDトークンを取得する
+async function fetchTokens(authorizationCode, clientSecret, logger) {
+  try {
+    const tokenResponse = await fetch('https://api.line.me/oauth2/v2.1/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        grant_type: 'authorization_code',
+        code: authorizationCode,
+        redirect_uri: REDIRECT_URI,
+        client_id: LINE_LOGIN_CHANNEL_ID,
+        client_secret: clientSecret,
+      }),
+    });
+
+    if (!tokenResponse.ok) {
+      const err = await tokenResponse.text();
+      logger.error(`Failed to fetch tokens: status: ${tokenResponse.statusText}, error: ${err}`);
+      return null;
+    }
+
+    const tokenData = await tokenResponse.json();
+    return tokenData;
+  } catch (error) {
+    logger.error(`Error fetching tokens: ${error.message}`);
+    throw error;
+  }
+}
+
+// IDトークンを使ってLINE IDを取得する
+async function verifyIdToken(idToken, logger) {
+  try {
+    const verifyResponse = await fetch('https://api.line.me/oauth2/v2.1/verify', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: new URLSearchParams({
+        id_token: idToken,
+        client_id: LINE_LOGIN_CHANNEL_ID,
+      }),
+    });
+
+    if (!verifyResponse.ok) {
+      const err = await verifyResponse.text();
+      logger.error(`Failed to verify token: status: ${verifyResponse.statusText}, error: ${err}`);
+      return null;
+    }
+
+    const verifyData = await verifyResponse.json();
+    return verifyData.sub;
+  } catch (error) {
+    logger.error(`Error verifying ID token: ${error.message}`);
+    throw error;
+  }
+}
+
+// 紐付けテーブルにデータを投入する
+async function upsertKarteRefTable(websiteUserId, lineId, logger) {
+  try {
+    const response = await sdk.postV2betaTrackReftableRowUpsert({
+      id: REF_TABLE_ID,
+      rowKey: { user_id: websiteUserId },
+      values: { line_id: lineId },
+    });
+
+    return response;
+  } catch (error) {
+    logger.error(`Error upserting KARTE ref table: ${error.message}`);
+    throw error;
+  }
+}
+
+export default async function (data, { MODULES }) {
+  const { initLogger, secret } = MODULES;
+  const logger = initLogger({ logLevel: LOG_LEVEL });
+
+  try {
+    const secrets = await secret.get({
+      keys: [KARTE_API_TOKEN_SECRET, LINE_CHANNEL_SECRET_NAME],
+    });
+
+    const karteToken = secrets[KARTE_API_TOKEN_SECRET];
+    const clientSecret = secrets[LINE_CHANNEL_SECRET_NAME];
+    sdk.auth(karteToken);
+
+    const { authorizationCode, websiteUserId } = data.jsonPayload.data.hook_data.body;
+
+    if (!authorizationCode || !websiteUserId) {
+      logger.warn('Missing authorizationCode or websiteUserId');
+      return {
+        craft_status_code: 400,
+        body: JSON.stringify({ message: 'Missing authorizationCode or websiteUserId' }),
+      };
+    }
+
+    const tokenData = await fetchTokens(authorizationCode, clientSecret, logger);
+    if (!tokenData) {
+      return {
+        craft_status_code: 400,
+        body: JSON.stringify({ message: 'Error in fetchTokens' }),
+      };
+    }
+
+    const { id_token: idToken } = tokenData;
+
+    const retrievedLineId = await verifyIdToken(idToken, logger);
+    if (!retrievedLineId) {
+      return {
+        craft_status_code: 401,
+        body: JSON.stringify({ message: 'Error in verifyIdToken' }),
+      };
+    }
+
+    await upsertKarteRefTable(websiteUserId, retrievedLineId, logger);
+
+    return {
+      craft_status_code: 200,
+      body: JSON.stringify({ message: 'Data inserted successfully' }),
+    };
+  } catch (err) {
+    logger.error(`Error in lineid mapping process: ${err.toString()}`);
+    return {
+      craft_status_code: 500,
+      body: JSON.stringify({ message: 'Internal Server Error' }),
+    };
+  }
+}
