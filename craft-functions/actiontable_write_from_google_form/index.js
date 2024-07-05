3a5f9192e72a1cdc0590ce72df94174e5e973686
soy <soichiro.yamaguchi@plaid.co.jp>
2024-07-05 19:54:24 +0900

    refactor: actiontable-write-from-googleform-http-type

--- craft-functions/actiontable_write_from_google_form/index.js
+++ craft-functions/actiontable_write_from_google_form/index.js
@@ -1,31 +1,44 @@
 import api from 'api';
-const LOG_LEVEL = '<% LOG_LEVEL %>'; // ログのレベルを必要に応じて定義
-const SECRET_NAME = '<% SECRET_NAME %>'; // 登録したシークレットを定義
-const karteApiClient = api('@dev-karte/v1.0#1ehqt16lkm2a8jw'); 
-const TABLE_ID = '<% TABLE_ID %>' // 今回更新したいテーブルIDを登録
+
+const LOG_LEVEL = '<% LOG_LEVEL %>';
+const SECRET_NAME = '<% SECRET_NAME %>';
+const karteApiClient = api('@dev-karte/v1.0#1ehqt16lkm2a8jw');
+const TABLE_ID = '<% TABLE_ID %>';
 
 export default async function (data, { MODULES }) {
-    const { initLogger, secret } = MODULES;
-    const logger = initLogger({ logLevel: LOG_LEVEL });
-    const secrets = await secret.get({ keys: [SECRET_NAME] });
-    const token = secrets[SECRET_NAME];
-    karteApiClient.auth(token);
+  const { initLogger, secret } = MODULES;
+  const { req, res } = data;
+  const logger = initLogger({ logLevel: LOG_LEVEL });
+  const secrets = await secret.get({ keys: [SECRET_NAME] });
+  const token = secrets[SECRET_NAME];
+  karteApiClient.auth(token);
 
-    const pageUrl = data.jsonPayload.data.hook_data.body.PageURL; // body.xxxの部分はGAS側の記述に依存して変更可能
-    const salesCount = data.jsonPayload.data.hook_data.body.SalesCount; 
-    const others = data.jsonPayload.data.hook_data.body.Others; 
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-    try {
-        await karteApiClient.postV2betaActionActiontableRecordsUpsert({
-            table: TABLE_ID,
-            data: {
-                pageurl: pageUrl, // ここの内容は作成したアクションテーブルのカラム名に応じて変更可能、今回の場合はpageurl, salescount, othersの3カラムにしたので左記のようになっています。
-                salescount: salesCount,
-                others
-            }
-        });
-        logger.log(`${TABLE_ID} is updated`);
-    } catch (e) {
-        logger.error(e);
-    }
+  if (req.method === 'OPTIONS') {
+    res.status(204).end();
+    return;
+  }
+
+  const pageUrl = req.body.PageURL;
+  const salesCount = req.body.SalesCount;
+  const others = req.body.Others;
+
+  try {
+    await karteApiClient.postV2betaActionActiontableRecordsUpsert({
+      table: TABLE_ID,
+      data: {
+        pageurl: pageUrl,
+        salescount: salesCount,
+        others,
+      },
+    });
+    logger.log(`${TABLE_ID} is updated`);
+    res.status(200).send({ message: 'Success' });
+  } catch (e) {
+    logger.error(e);
+    res.status(500).send({ message: 'Error occurred' });
+  }
 }
